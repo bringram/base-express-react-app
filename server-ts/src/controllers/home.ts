42e1e2ee3fe0ac64c48035310e011e1a5624bbb8
bringram <brandongingram@gmail.com>
2020-02-07 20:51:00 -0500

    Reformatted code to adhere to standard of the rest of the app

--- server-ts/src/controllers/home.ts
+++ server-ts/src/controllers/home.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from 'express';
 const homeController = Router();
 
 homeController.get('/', (req: Request, res: Response) => {
-    res.send('Hello world!');
+  res.send('Hello world!');
 });
 
 export default homeController;
